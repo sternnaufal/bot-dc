7ef59980db82dabdfde8930226cb8210da40a379
Naufal Rakha Putra <84272443+sternnaufal@users.noreply.github.com>
2023-01-31 19:02:25 +0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,52 @@
+//in bot.js
+
+const { ask } = require("./ai.js"); //import the "ask" function from the "ai.js" file
+const { Client, Events, GatewayIntentBits, ActivityType, Message} = require('discord.js'); //v14.6.0
+const token = "MTA2OTY1MzQzMjE0ODk2NzUwNA.G1gW-K.5PZSvaeCmcQB7Bf5a5pu4bKuu5belElbajJ0Tw";
+
+// Create a new client instance
+const client = new Client({
+  
+  intents:
+    [GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent]
+});
+client.on('ready' , () =>{
+  console.log('Bot Online');
+  client.user.setStatus('online');
+  console.log('bot status', client.user.presence.status);
+  client.user.setActivity(` on ${client.guilds.cache.size} servers. Prefix : ask?`, { type: ActivityType.Watching });
+
+ });
+  // client.user is now defined
+
+client.once(Events.ClientReady, c => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+client.on(Events.MessageCreate, async message => {
+  if (message.content === "ping") {
+    message.reply("pong");
+  }
+});
+client.on(Events.MessageCreate, async message => {
+  if (message.content === "ask?Kamu suka siapa?") {
+    message.reply("aku suka <@997641782173126676>");
+  }
+});
+client.on(Events.MessageCreate, async message => {
+  if (message.content === "halo") {
+    message.reply("ID : Hello aku Hanekawa-bot, tanyakan saja padaku! \nEN : Hi, I'm Hanekawa-bot, ask me everything! \nPrefix : ask?");
+  }
+});
+
+client.on(Events.MessageCreate, async message => {
+  if (message.content.substring(0, 4) === "ask?") {
+    const prompt = message.content.substring(4);
+    const answer = await ask(prompt);
+    message.reply(answer);
+  }
+});
+
+// Log in to Discord with your client's token
+client.login(token);
