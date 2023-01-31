90372d891dd738412cd8121831a31891b06a83f5
Naufal Rakha Putra <84272443+sternnaufal@users.noreply.github.com>
2023-01-31 19:06:35 +0700

    Create ai.js

---
+++ ai.js
@@ -0,0 +1,23 @@
+
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+    apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+async function ask(prompt) {
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt,
+        temperature: 0.7,
+        max_tokens: 500,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    });
+    const answer = response.data.choices[0].text;
+    return answer;
+}
+
+module.exports = {
+    ask,
+};
